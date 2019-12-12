eb0b78f3096de4ed2e6181dd45e356c72c264767
excalibur-sword <58806959+excalibur-sword@users.noreply.github.com>
2019-12-12 22:13:30 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,6 +32,40 @@ template <> void PrintRump<df80>(void) {
   printf("Rump<double_float_80>: %+1.9e\n", Rump<df80>().Hi().ToDouble());
 }
 
+const double F64_EPS = 2.2204460492503131e-16;
+const int SIN_TAYLOR_SZ = 15;
+
+// Computes sin(x) using Taylor series. Assumes |x| <= pi/32.
+f80 SinTaylor(const f80 &x) {
+  if (x.IsZero()) return x;
+
+  f80 x2 = -x*x, s = x, t = x;
+  double threshold = 0.5*Abs(x).ToDouble()*F64_EPS;
+  for (int i = 1; i < SIN_TAYLOR_SZ; ++i) {
+    t /= 2*i*(2*i + 1);
+    t *= x2;
+    s += t;
+    printf("%2d = %+1.14e\n", i, s.ToDouble());
+    if (threshold > Abs(t).ToDouble()) break;
+  }
+  return s;
+}
+
+df80 SinTaylor(const df80 &x) {
+  if (x.IsZero()) return x;
+
+  df80 x2 = -x*x, s = x, t = x;
+  f80 threshold = 0.5*Abs(x.Hi())*F80_EPS;
+  for (int i = 1; i < SIN_TAYLOR_SZ; ++i) {
+    t /= 2*i*(2*i + 1);
+    t *= x2;
+    s += t;
+    printf("%2d = %+1.14e %+1.14e\n", i, s.Hi().ToDouble(), s.Lo().ToDouble());
+    if (threshold > Abs(t.Hi())) break;
+  }
+  return s;
+}
+
 int main(void) {
   // clear flush-to-zero, and
   // clear denormals-are-zero mode
@@ -47,6 +81,13 @@ int main(void) {
   PrintRump<long double>(); // with compiler option /Qlong-double
   PrintRump<f80>();
   PrintRump<df80>();
+
+  const f80 x = F80_PI/40;
+  printf("\nsinus<float_80> using Taylor series\n");
+  SinTaylor(x);
+
+  printf("\nsinus<double_float_80> using Taylor series\n");
+  SinTaylor((df80)x);
 
   return 0;
 }
